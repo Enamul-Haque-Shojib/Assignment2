4cd9ab783e7f9e269a0b205cf71ea214d4bc7569
Enamul-Haque-Shojib <enamulhaqueshojib@gmail.com>
2026-05-21 14:34:46 +0600

    types added

---
+++ src/types/express.d.ts
@@ -0,0 +1,22 @@
+
+
+declare global {
+
+  namespace Express {
+
+    interface Request {
+
+      user: {
+        id: number;
+        name: string;
+        email: string;
+        role: ROLES;
+      };
+
+    }
+
+  }
+
+}
+
+export {};
---
+++ src/types/index.ts
@@ -0,0 +1,39 @@
+export const USER_ROLE = {
+  contributor: "contributor",
+  maintainer: "maintainer",
+} as const;
+
+export type ROLES = typeof USER_ROLE[keyof typeof USER_ROLE];
+
+export type RegisterPayload = {
+  name: string;
+  email: string;
+  password: string;
+  role?: ROLES;
+};
+
+export type GetIssuesQuery = {
+  sort?: "newest" | "oldest";
+  type?: "bug" | "feature_request";
+  status?: "open" | "in_progress" | "resolved";
+};
+
+
+export type UpdateIssuePayload = {
+  title?: string;
+  description?: string;
+  type?: "bug" | "feature_request";
+};
+
+export type CurrentUser = {
+  id: number;
+  role: ROLES;
+};
+
+
+// export type JwtUserPayload = {
+//   id: number;
+//   name: string;
+//   email: string;
+//   role: ROLES;
+// };
